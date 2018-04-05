d1750fa88512ea580415a4e33250d2f95e1efbf8
Marcus Larsson <marcuslarsson_92@hotmail.com>
2018-04-05 18:26:52 +0200

    return default constucted T for none specialized query::get_column()

--- include/sqlitecxx/statement.h
+++ include/sqlitecxx/statement.h
@@ -72,7 +72,7 @@ class query : public detail::statement {
     bool step() { return sqlite3_step(data()) == sqlite::row; }
 
     template <typename T>
-    inline T get_column(std::size_t index) const = delete;
+    T get_column(std::size_t) const { return T(); }
 };
 
 template <>
